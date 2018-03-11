1222d91cfb9d4c1e8e200fa5d590bd5f93c505a5
Benny Hwang <bennyhwangg@gmail.com>
2018-03-11 12:06:12 +0000

    db change

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ var commentRoutes    = require("./routes/comments"),
     indexRoutes      = require("./routes/index");
 
 //Using packages
-//mongoose.connect(process.env.DATAURL); // testing database
+mongoose.connect(process.env.DATAURL); // testing database
 // mongoose.connect("mongodb://localhost/j_food");
 // // mongoose.connect("mongodb://jblogadmin:blog415@ds253468.mlab.com:53468/jfoodblog")
 app.use(bodyParser.urlencoded({ extended: true }));
